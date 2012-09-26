691736b9a228a72789aa01394f6434e8c83948bf
siddick <mebramsha@paypal.com>
2012-09-26 17:28:29 +0530

    Added simplecov gem to find code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,11 @@
 require 'bundler/setup'
-Bundler.require :default, :test
 
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter "/spec/"
+  end
+end
+
+Bundler.require :default, :test
 PayPal::SDK::Core::Config.load('spec/config/paypal.yml', 'test')
