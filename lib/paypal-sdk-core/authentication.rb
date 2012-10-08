82c1eff516cd27ddc57ec7431aa7de7f29ae3e32
siddick <mebramsha@paypal.com>
2012-10-08 10:28:22 +0530

    document updated for authentication and logger

--- lib/paypal-sdk-core/authentication.rb
+++ lib/paypal-sdk-core/authentication.rb
@@ -1,14 +1,24 @@
 require 'paypal-sdk-core/config'
 
 module PayPal::SDK::Core
+  
+  # Contains methods to format credentials for HTTP protocol.
+  # == Example
+  #  include Authentication
+  #  add_header(request)
+  #  add_certificate(http)
   module Authentication
     
+    # Load configuration when 
     def self.included(klass)
       klass.class_eval do
         include Configuration
       end
     end
     
+    # Set or Get HTTP authentication Header.
+    # === Arguments 
+    # * <tt>request</tt> -- HTTP Request object.
     def add_headers(request = {})
       request["X-PAYPAL-SECURITY-USERID"]     = config.username
       request["X-PAYPAL-SECURITY-PASSWORD"]   = config.password
@@ -16,7 +26,10 @@ module PayPal::SDK::Core
       request["X-PAYPAL-SECURITY-SIGNATURE"]  = config.signature  if config.signature
       request
     end
-            
+    
+    # Configure ssl certificate to HTTP object
+    # === Argument
+    # * <tt>http</tt> -- Net::HTTP object         
     def add_certificate(http = self)
       if config.cert_path and http
         cert_content = File.read(config.cert_path)
--- lib/paypal-sdk-core/logging.rb
+++ lib/paypal-sdk-core/logging.rb
@@ -1,8 +1,16 @@
 require 'logger'
 
 module PayPal::SDK::Core
+  # Include Logging module to provide logger functionality. 
+  # == Configure logger
+  #   Logging.logger = Logger.new(STDERR)
+  #   
+  # == Example
+  #   include Logger
+  #   logger.info "Debug message"
   module Logging
   
+    # Get logger object
     def logger
       @logger ||= Logging.logger_for(self.class.name)
     end
@@ -11,21 +19,31 @@ module PayPal::SDK::Core
     @loggers  = {}
   
     class << self
-  
+
+      # Get or Create logger object based on Class  
       def logger_for(classname)
         @loggers[classname] ||= configure_logger_for(classname)
       end
   
+      # Create logger object for the given class
+      # === Argument
+      # * <tt>classname</tt> -- Class name for logger to create
       def configure_logger_for(classname)
         new_logger = logger.dup
         new_logger.progname = classname
         new_logger
       end
-      
+
+      # Get or Create configured logger based on the default environment configuration       
       def logger
         @logger ||= Logger.new(Config.config.logfile || STDERR)
       end
       
+      # Set logger directly and clear the loggers cache.
+      # === Attributes
+      # * <tt>logger</tt> -- Logger object
+      # === Example
+      #   Logging.logger = Logger.new(STDERR)
       def logger=(logger)
         @loggers = {}
         @logger  = logger
