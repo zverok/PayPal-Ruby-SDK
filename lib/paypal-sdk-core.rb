f0a9c00ae4fc364ca327c353f4aa85acc6a0238e
siddick <mebramsha@paypal.com>
2012-10-17 11:40:43 +0530

    Rename directory paypal-sdk-core to paypal-sdk/core

--- lib/paypal-sdk-core.rb
+++ lib/paypal-sdk-core.rb
@@ -1,31 +1,31 @@
-require "paypal-sdk-core/version"
-require "paypal-sdk-core/config"
-require "paypal-sdk-core/logging"
+require "paypal-sdk/core/version"
+require "paypal-sdk/core/config"
+require "paypal-sdk/core/logging"
 
 module PayPal
   module SDK
     module Core
       
-      autoload :Authentication, "paypal-sdk-core/authentication"
+      autoload :Authentication, "paypal-sdk/core/authentication"
       
       module API
-        autoload :Base,     "paypal-sdk-core/api/base"
-        autoload :Merchant, "paypal-sdk-core/api/merchant"
-        autoload :Platform, "paypal-sdk-core/api/platform"
+        autoload :Base,     "paypal-sdk/core/api/base"
+        autoload :Merchant, "paypal-sdk/core/api/merchant"
+        autoload :Platform, "paypal-sdk/core/api/platform"
       end
       
       module Util
-        autoload :OauthSignature, "paypal-sdk-core/util/oauth_signature"
+        autoload :OauthSignature, "paypal-sdk/core/util/oauth_signature"
       end
       
       module Credential
-        autoload :Base,         "paypal-sdk-core/credential/base"
-        autoload :Certificate,  "paypal-sdk-core/credential/certificate"
-        autoload :Signature,    "paypal-sdk-core/credential/signature"
+        autoload :Base,         "paypal-sdk/core/credential/base"
+        autoload :Certificate,  "paypal-sdk/core/credential/certificate"
+        autoload :Signature,    "paypal-sdk/core/credential/signature"
         
         module ThirdParty
-          autoload :Token,    "paypal-sdk-core/credential/third_party/token"
-          autoload :Subject,  "paypal-sdk-core/credential/third_party/subject"
+          autoload :Token,    "paypal-sdk/core/credential/third_party/token"
+          autoload :Subject,  "paypal-sdk/core/credential/third_party/subject"
         end
       end
       
--- paypal-sdk-core.gemspec
+++ paypal-sdk-core.gemspec
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'paypal-sdk-core/version'
+require 'paypal-sdk/core/version'
 
 Gem::Specification.new do |gem|
   gem.name          = "paypal-sdk-core"
