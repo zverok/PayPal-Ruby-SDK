f564c294b468cd2c2ed79caa28bddb660b707b52
siddick <mebramsha@paypal.com>
2012-11-15 13:51:45 +0530

    Define constants for the Enum values

--- lib/paypal-sdk/core/api/data_types/enum.rb
+++ lib/paypal-sdk/core/api/data_types/enum.rb
@@ -9,9 +9,13 @@ module PayPal::SDK::Core
           end
 
           def options=(options)
-            @options = options
-            @options.each do |option|
-              const_set(option, option)
+            if options.is_a? Hash
+              options.each do |const_name, value|
+                const_set(const_name, value)
+              end
+              @options = options.values
+            else
+              @options = options
             end
           end
         end
