b50b397226908ac226e7970cf26a830d9512b4be
siddick <mebramsha@paypal.com>
2013-04-24 14:43:38 +0530

    Created files for API, Credential and Util modules

--- lib/paypal-sdk-core.rb
+++ lib/paypal-sdk-core.rb
@@ -9,38 +9,9 @@ module PayPal
       autoload :Authentication, "paypal-sdk/core/authentication"
       autoload :Exceptions,     "paypal-sdk/core/exceptions"
       autoload :OpenIDConnect,  "paypal-sdk/core/openid_connect"
-
-      module API
-        autoload :Base,     "paypal-sdk/core/api/base"
-        autoload :Merchant, "paypal-sdk/core/api/merchant"
-        autoload :Platform, "paypal-sdk/core/api/platform"
-        autoload :REST,     "paypal-sdk/core/api/rest"
-        autoload :IPN,      "paypal-sdk/core/api/ipn"
-
-        module DataTypes
-          autoload :Base, "paypal-sdk/core/api/data_types/base"
-          autoload :Enum, "paypal-sdk/core/api/data_types/enum"
-          autoload :SimpleTypes,    "paypal-sdk/core/api/data_types/simple_types"
-          autoload :ArrayWithBlock, "paypal-sdk/core/api/data_types/array_with_block"
-        end
-      end
-
-      module Util
-        autoload :OauthSignature, "paypal-sdk/core/util/oauth_signature"
-        autoload :OrderedHash,    "paypal-sdk/core/util/ordered_hash"
-        autoload :HTTPHelper,     "paypal-sdk/core/util/http_helper"
-      end
-
-      module Credential
-        autoload :Base,         "paypal-sdk/core/credential/base"
-        autoload :Certificate,  "paypal-sdk/core/credential/certificate"
-        autoload :Signature,    "paypal-sdk/core/credential/signature"
-
-        module ThirdParty
-          autoload :Token,    "paypal-sdk/core/credential/third_party/token"
-          autoload :Subject,  "paypal-sdk/core/credential/third_party/subject"
-        end
-      end
+      autoload :API,            "paypal-sdk/core/api"
+      autoload :Util,           "paypal-sdk/core/util"
+      autoload :Credential,     "paypal-sdk/core/credential"
 
     end
 
